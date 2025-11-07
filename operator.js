cd3193debaf1201ddf11d3e1faf2921bddef5ba4
nadia419 <nadiasultananisu12@gmail.com>
2025-11-07 17:08:09 +0100

    Create operator.js with JavaScript operators overview
    Added an HTML document explaining JavaScript operators.

---
+++ operator.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Operators in js</h1>
+    <p>
+        +(plus) number + string(the + in strings equals a contention)
+        -(minus) number
+        +(multiply) number
+        / (division) number
+        ** (exponentiation) number
+        % (mod or remainder of division) number
+        > < >= <= (comparison operators) (can only return boolean values)
+    </p>
+</body>
+</html>
